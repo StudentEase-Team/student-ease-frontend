c4bac801c810ce6ed92a43471dac2ef78b562844
Ognjen Milojevic <ognjenmilojevic2001@gmail.com>
2024-08-03 14:52:15 +0200

    fix: misc fixes for map

--- studentease/app/(app)/map.tsx
+++ studentease/app/(app)/map.tsx
@@ -3,21 +3,26 @@ import { Platform, View, StyleSheet } from 'react-native';
 import { Canvas, useThree } from '@react-three/fiber';
 import { Tank } from '../../component/3d/Tank';
 import { GestureHandlerRootView, PanGestureHandler, PinchGestureHandler } from 'react-native-gesture-handler';
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
 import * as THREE from 'three';
+import useControls from "r3f-native-orbitcontrols"
 
 const Map = () => {
   const cameraRef = useRef<THREE.PerspectiveCamera | THREE.OrthographicCamera | null>(null);
 
   const onPanGestureEvent = (event) => {
+    console.info("pan " + cameraRef.current);
     if (cameraRef.current) {
       const { translationX, translationY } = event.nativeEvent;
+
       // Move camera position
-      cameraRef.current.position.x -= translationX / 1000;
-      cameraRef.current.position.y += translationY / 1000;
+      cameraRef.current.position.x -= translationX / 10000;
+      cameraRef.current.position.y += translationY / 10000;
     }
   };
 
   const onPinchGestureEvent = (event) => {
+    console.info("pinch");
     if (cameraRef.current) {
       const { scale } = event.nativeEvent;
       if (cameraRef.current instanceof THREE.PerspectiveCamera) {
@@ -38,22 +43,37 @@ const Map = () => {
     return null;
   };
 
+  if(Platform.OS !== 'web')
   return (
     <GestureHandlerRootView style={styles.container}>
+    <PinchGestureHandler onGestureEvent={onPinchGestureEvent}>
       <PanGestureHandler onGestureEvent={onPanGestureEvent}>
-        <PinchGestureHandler onGestureEvent={onPinchGestureEvent}>
-          <View style={styles.container}>
+
             <Canvas>
               <CameraSetup />
               <ambientLight intensity={0.5} />
               <pointLight position={[10, 10, 10]} intensity={1} />
               <Tank />
             </Canvas>
-          </View>
+          </PanGestureHandler>
         </PinchGestureHandler>
-      </PanGestureHandler>
+
     </GestureHandlerRootView>
   );
+  else{
+  const [OrbitControls, events] = useControls()
+  return (
+      <View {...events} style={styles.container}>
+        <Canvas>
+          <OrbitControls/>
+          <CameraSetup />
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} intensity={1} />
+          <Tank />
+        </Canvas>
+      </View>
+  )
+  }
 };
 
 const styles = StyleSheet.create({
