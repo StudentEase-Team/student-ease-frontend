c3b7ed966ce174f7bed9521db6e20ee6bdd4789a
Ognjen Milojevic <ognjenmilojevic2001@gmail.com>
2024-08-13 22:24:07 +0200

    feat: linked with map server

--- studentease/app/(app)/map.tsx
+++ studentease/app/(app)/map.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Platform, View, StyleSheet, requireNativeComponent } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { Canvas } from '@react-three/fiber';
-import { NastavniBlokJedan } from '../../component/3d/NastavniBlokJedan';
+//import { NastavniBlokJedan } from '../../component/3d/NastavniBlokJedan';
 import useControls from 'r3f-native-orbitcontrols';
 
 const Map = () => {
   return Platform.OS === "web" ? (
-    <iframe src="http://192.168.0.23:3000/" height={'100%'} width={'100%'} />
+    <iframe src="http://192.168.0.23:3000/nastavniblok/1" height={'100%'} width={'100%'} />
   ):(
       <WebView
-        source={{ uri: 'http://192.168.0.23:3000/' }} // Replace with your actual URL
+        source={{ uri: 'http://192.168.0.23:3000/nastavniblok/1' }} // Replace with your actual URL
         style={styles.container}
       />
     );
@@ -21,7 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     height:'100%',
     width:'100%',
-    backgroundColor:'red'
   },
 });
 
